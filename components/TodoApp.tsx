f57b1df72c9b43d248e4a3fdc8d82d739ea9c9f6
macha <kazuki.aishima.dev@gmail.com>
2023-03-16 22:47:42 +0900

    create supabase conector

--- components/TodoApp.tsx
+++ components/TodoApp.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Todo } from '../utils/interface';
+import { getAllTodos } from '../utils/supabasefunction';
 import TodoList from './TodoList'
 
 const TodoApp = () => {
+  const [todos, setTodos] = useState<any>([]);
+
+  useEffect(() => {
+    const getTodos = async () => {
+      const todos = await getAllTodos();
+      setTodos(todos);
+      console.log(todos);
+    };
+    getTodos();
+  }, []);
+
   return (
     <section className='text-center mb-2 text-2xl font-medium'>
         <h3>Supabase TodoApp</h3>
@@ -9,7 +22,7 @@ const TodoApp = () => {
             <input type="text" className='mr-2 shadow-lg p-1 outline-none'/>
             <button className='shadow-md border-2 px-1 py-2 rounded-lg bg-green-500'>Add</button>
         </form>
-        <TodoList />
+        <TodoList todos={todos} />
     </section>
   )
 }
